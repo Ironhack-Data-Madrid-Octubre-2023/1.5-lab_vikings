f7d7acfcf31c5db7db186c10f6f03b037d741305
Carolina Morales <carolina.morales.hndz@gmail.com>
2023-10-13 17:27:57 +0200

    ejercicios hechos hasta antes de war

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -16,17 +16,49 @@ class Soldier:
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+
+    def __init__(self, name, health, strength):
+
+        Soldier.__init__(self, health, strength)
+        self.name=name
+
+    def receiveDamage(self, damage):
+
+        self.health -= damage
+        if self.health > 0:
+            return (f'{self.name} has received {damage} points of damage')
+            
+        elif self.health <= 0:
+            return (f'{self.name} has died in act of combat')
+     
+    def battleCry(self):
+        return "Odin Owns You All!"
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+
+    def __init__(self, health, strength):
+        Soldier.__init__(self, health, strength)
+
+    def receiveDamage(self, damage):
+
+        self.health -= damage
+
+        if self.health > 0:
+            return(f'A Saxon has received {damage} points of damage')
+        else:
+            return "A Saxon has died in combat"
 
 # War
 
 
 class War:
-    pass
+
+    def __init__(self):
+        self.vikingArmy=[]
+        self.saxonArmy=[]
+
+    def addVikin(self):
