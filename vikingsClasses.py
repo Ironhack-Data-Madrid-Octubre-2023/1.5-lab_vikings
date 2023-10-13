f6000960d40f1789102379ce427cfabc3144e7f2
Miguel Cuadros <miguelcuadrosmoronta@gmail.com>
2023-10-13 17:35:48 +0200

    A ver si esto funciona

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,24 +1,95 @@
+import random
 
 # Soldier
 
 
 class Soldier:
-    pass
+
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage):
+        self.health -= damage
+        
+
+    
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+
+    def __init__(self, name):
+        
+        self.name = name
+        
+        
+    def receiveDamage(self, damage):
+        if self.health > 0:
+            self.health = self.health - damage
+
+        elif self.health == 0:
+            return (f'{self.name} has died in act of combat')
+    
+
+
+    def battleCry(self):
+        return ("Odin Owns You All!")
+
+
+    
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+
+    def receiveDamage(self, damage):
+        
+        if self.health > damage:
+            self.health = self.health - damage
+            
+            return (f'A Saxon has received {damage} points of damage')
+        
+        elif self.health <= damage:
+            return ('A Saxon has died in combat')
+
+
+
+
 
 # War
 
 
-class War:
-    pass
+vikingArmy = []
+saxonArmy = []
+
+class War():
+    
+    
+    def __init__(self):
+        vikingArmy.append([])
+        saxonArmy.append([])
+
+    def addViking(self, Viking):
+        for place in vikingArmy:
+            vikingArmy.append(Viking)
+
+
+    def addSaxon(self, Saxon):
+        for place in saxonArmy:
+            saxonArmy.append(Saxon)
+
+    def vikingAttack(self):
+        saxon = random.choice(saxonArmy)
+
+    def saxonAttack():
+        pass
+
+    def showStatus():
+        pass
+    
