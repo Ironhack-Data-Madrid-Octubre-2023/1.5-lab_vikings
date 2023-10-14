5882c509c341e8bc3a25d4add381c03ca6d789c6
Kohkitos <sanzyagain@gmail.com>
2023-10-14 10:46:07 +0200

    Saxon finished

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -24,7 +24,7 @@ class Viking(Soldier):
         self.name = name
 
     def receiveDamage(self, damage):
-        self.health -= damage
+        Soldier.receiveDamage(self, damage)
         if self.health > 0:
             return (f"{self.name} has received {damage} points of damage")
         else:
@@ -36,8 +36,16 @@ class Viking(Soldier):
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        Soldier.__init__(self, health, strength)
+
+    def receiveDamage(self, damage):
+        Soldier.receiveDamage(self, damage)
+        if self.health > 0:
+            return (f"A Saxon has received {damage} points of damage")
+        else:
+            return ("A Saxon has died in combat")
 
 # War
 
